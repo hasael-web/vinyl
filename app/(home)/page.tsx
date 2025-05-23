2d0398612693c743dec9137f4a2381b9dd8b6cc4
hasael-web <hasaelbutarbutar80@gmail.com>
2025-05-23 16:25:16 +0700

    update size

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -7,7 +7,7 @@ import DiscCover from "./__components/disc-cover";
 export default function Home() {
   return (
     <div className="h-screen w-screen ">
-      <Canvas camera={{ position: [0, 0, 10], fov: 18 }}>
+      <Canvas camera={{ position: [0, 0, 10], fov: 22 }}>
         <ambientLight intensity={0.01} />
         <DiscAnimation />
         <DiscCover />
